927963d69fb1653cbbcd2941a5e5ff349fc2e034
alpertas <alpertas.cpp@gmail.com>
2023-05-14 01:57:05 +0300

    feat: provided store to App.tsx

--- App.tsx
+++ App.tsx
@@ -7,13 +7,20 @@ import {
   Text,
   View,
 } from 'react-native';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './src/redux/store';
+
+const store = createStore(rootReducer);
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle={'light-content'} />
-      <Text>Hello world!</Text>
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle={'light-content'} />
+        <Text>Hello world!</Text>
+      </SafeAreaView>
+    </Provider>
   );
 }
 
