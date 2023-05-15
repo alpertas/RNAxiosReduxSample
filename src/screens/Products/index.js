6d1a81c5ef5c90b109942703595107f841abc4a0
alpertas <alpertas.cpp@gmail.com>
2023-05-16 02:39:19 +0300

    feat: dispatched products

--- src/screens/Products/index.js
+++ src/screens/Products/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View, FlatList} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 
 // Styles
 import styles from './styles/AllProductStyles';
@@ -8,9 +9,43 @@ import styles from './styles/AllProductStyles';
 import {getAllProducts} from '../../redux/actions/productActions';
 
 const AllProducts = () => {
+  const dispatch = useDispatch();
+
+  const products = useSelector(state => state.product.products);
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, [dispatch]);
+
+  const renderProductItem = ({item}) => (
+    <View style={styles.productItem}>
+      <Text>
+        <Text style={styles.label}>ID: </Text>
+        {item.id}
+      </Text>
+      <Text>
+        <Text style={styles.label}>Name: </Text>
+        {item.name}
+      </Text>
+      <Text>
+        <Text style={styles.label}>Price: </Text>
+        {item.price}
+      </Text>
+      <Text>
+        <Text style={styles.label}>Description: </Text>
+        {item.description}
+      </Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <Text>AllProductsPage</Text>
+      <FlatList
+        data={products}
+        renderItem={renderProductItem}
+        keyExtractor={item => item.id.toString()} // Convert id to string for keyExtractor
+      />
     </View>
   );
 };
--- src/screens/Products/styles/AllProductStyles.js
+++ src/screens/Products/styles/AllProductStyles.js
@@ -3,8 +3,21 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  productItem: {
+    marginBottom: 16,
+    backgroundColor: '#f2f2f2',
+    padding: 8,
+    borderRadius: 4,
+  },
+  label: {
+    fontWeight: 'bold',
   },
 });
 
