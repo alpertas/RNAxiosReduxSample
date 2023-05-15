a5f5fd942acadd195ab081c3894665c722ca7521
alpertas <alpertas.cpp@gmail.com>
2023-05-16 02:38:38 +0300

    fix: added try-catch expressions

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,15 +1,24 @@
 import axios from 'axios';
+import Config from 'react-native-config';
 import {SET_ALL_PRODUCTS} from '../constants/action-types';
+
+/* TODO 
+- figure out env file.
+*/
+const process = Config.REACT_APP_BASE_URL;
+// REACT_APP_BASE_URL =https://dummyjson.com/
 
 export const getAllProducts = () => {
   return async dispatch => {
-    await axios
-      .get(process.env.REACT_APP_BASE_URL + 'products')
-      .then(response => {
-        dispatch({
-          type: SET_ALL_PRODUCTS,
-          payload: response.data,
-        });
+    try {
+      const response = await axios.get('https://dummyjson.com/products');
+      dispatch({
+        type: SET_ALL_PRODUCTS,
+        payload: response.data,
       });
+    } catch (error) {
+      // Handle the error here
+      console.error('An error occurred while fetching products:', error);
+    }
   };
 };
